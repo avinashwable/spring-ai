12df9d41ee962c3b57f0be2ded9b931132ede24e
Avinash Wable <avinash.wable@emburse.com>
2026-01-14 16:26:42 +0530

    Chat with memory, LLMs are stateless by default

--- src/main/java/com/avi/spring/aiworkshop/memory/MemoryController.java
+++ src/main/java/com/avi/spring/aiworkshop/memory/MemoryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// chat with memory using ChatClient configured to use ChatMemory
 @RestController
 @RequestMapping("/memory")
 public class MemoryController {
