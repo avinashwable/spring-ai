dd69462caa77f803828f8494b200256873532d10
Avinash Wable <avinash.wable@emburse.com>
2026-01-14 16:26:23 +0530

    Chat with memory, LLMs are stateless by default

--- src/main/java/com/avi/spring/aiworkshop/AiModelsConfig.java
+++ src/main/java/com/avi/spring/aiworkshop/AiModelsConfig.java
@@ -2,6 +2,8 @@ package com.avi.spring.aiworkshop;
 
 import org.springframework.ai.anthropic.AnthropicChatModel;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,14 @@ public class AiModelsConfig {
     @Bean
     public ChatClient openAiChatClient(OpenAiChatModel chatModel) {
         return ChatClient.create(chatModel);
+    }
+
+    @Bean
+    public ChatClient openAiChatClientWithMemory(OpenAiChatModel chatModel, ChatMemory chatMemory) {
+
+        return ChatClient.builder(chatModel)
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     @Bean
---
+++ src/main/java/com/avi/spring/aiworkshop/memory/MemoryController.java
@@ -0,0 +1,38 @@
+package com.avi.spring.aiworkshop.memory;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/memory")
+public class MemoryController {
+
+    private final ChatClient chatClient;
+    private final ChatClient chatClientWithMemory;
+
+    public MemoryController(@Qualifier("openAiChatClient") ChatClient chatClient,
+                            @Qualifier("openAiChatClientWithMemory") ChatClient chatClientWithMemory) {
+        this.chatClient = chatClient;
+        this.chatClientWithMemory = chatClientWithMemory;
+    }
+
+    @GetMapping("/chat")
+    public String normalPrompt(@RequestParam String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+    @GetMapping("/chat-memory")
+    public String promptWithMemory(@RequestParam String message) {
+        return chatClientWithMemory.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+}
